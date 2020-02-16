0281b68861ad9b1939f7632872736772c8c851ed
Ariel Biernacki <arielbiernacki@gmail.com>
2020-02-16 20:03:28 +0100

    Rename dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js'
   },
   module: {
